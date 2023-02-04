d5e2842d9053ba3033d3a7ae906fb5136c00cb1e
cristian <camunoz2@gmail.com>
2023-02-04 19:05:13 -0300

    added sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      backgroundColor: {
-        "lnsf-yellow": "#FFD059",
-        "lnsf-blue": "#0266FF",
-      },
       colors: {
         "lnsf-yellow": "#FFD059",
         "lnsf-blue": "#0266FF",
+        "lnsf-light": "#efefef",
       },
     },
   },
