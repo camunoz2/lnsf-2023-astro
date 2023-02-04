ceed63024da6858e81a44e5a23a74261ce06d58f
cristian <camunoz2@gmail.com>
2023-02-04 19:41:51 -0300

    calendar desktop view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
         "lnsf-yellow": "#FFD059",
         "lnsf-blue": "#0266FF",
         "lnsf-light": "#efefef",
+        "lnsf-light-text": "#7a7a7a",
+      },
+      boxShadow: {
+        lnsf: "0px 27px 41px -14px rgba(2, 102, 255, 0.13)",
       },
     },
   },
